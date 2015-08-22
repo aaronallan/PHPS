100c083df6321480ed88d774c7e43913747f275a
AaronAllan <aaron.a.pollock@gmail.com>
2015-08-22 15:53:38 -0400

    wrapped functions in IIFE

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,18 +1,9 @@
-$(document).ready(function () {
-  console.log('page rendered');
-});
-
-$('li').on('click', function() {
-  $(this).addClass('active');
-  $('li').not(this).removeClass('active');
-  console.log($(this).attr('value'));
-  $('#content').load('views/view.html #' + $(this).attr('value'));
-  return false;
-});
-
-// $('li.active').change(function() {
-//   console.log($('li.active').val());
-//   // $('#content').load('#' + $('li.active').val());
-
-//   return false;
-// });
+(function(jQ) {
+  jQ('.menu-item').on('click', function() {
+    jQ(this).addClass('active');
+    jQ('.menu-item').not(this).removeClass('active');
+    console.log(jQ(this).attr('value'));
+    jQ('#content').load('views/view.html #' + jQ(this).attr('value'));
+    return false;
+  });
+})(jQuery) 
