63cabdd0b0c3ec9e057d1a9f4fcd52d17018d491
AaronAllan <aaron.a.pollock@gmail.com>
2015-08-22 17:18:02 -0400

    removed console log

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,9 +2,7 @@
   jQ('.menu-item').on('click', function() {
     jQ(this).addClass('active');
     jQ('.menu-item').not(this).removeClass('active');
-    console.log(jQ(this).attr('value'));
     jQ('#content').load('views/view.html #' + jQ(this).attr('value'));
-    return false;
   });
 
   jQ('.hamburger').on('click', function () {
