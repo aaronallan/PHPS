9731167192d43060206e9c2e91301c4488b258c4
AaronAllan <aaron.a.pollock@gmail.com>
2015-08-23 12:20:19 -0400

    updated mobile menu to close on click

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,15 +1,18 @@
 (function(jQ) {
+  //On Ready, load home view
   jQ(document).on('ready', function (){
     jQ('#content').load('views/view.html #index');
   });
 
+  //On List Item Click, update menu and load appropriate section
   jQ('.menu-item').on('click', function() {
     jQ(this).addClass('active');
     jQ('.menu-item').not(this).removeClass('active');
     jQ('#content').load('views/view.html #' + jQ(this).attr('value'));
   });
 
-  jQ('.hamburger').on('click', function () {
+  //Toggle Mobile Menu
+  jQ('.hamburger, .mobile-menu ul li').on('click', function () {
     jQ('.mobile-menu').slideToggle();
   });
 })(jQuery) 
