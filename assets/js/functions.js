f5541c21a8629badff13bc7e1ea6e00cb60e7c4f
AaronAllan <aaron.a.pollock@gmail.com>
2015-09-02 21:25:51 -0400

    updated $

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,24 +1,24 @@
-(function(jQ) {
+(function($) {
   //On Ready, load home view
-  jQ(document).on('ready', function (){
-    jQ('#content').load('views/view.html #index');
+  $(document).on('ready', function (){
+    $('#content').load('views/view.html #index');
   });
 
   //On List Item Click, update menu and load appropriate section
-  jQ('.menu-item').on('click', function() {
-    jQ(this).addClass('active');
-    jQ('.menu-item').not(this).removeClass('active');
-    jQ('#content').load('views/view.html #' + jQ(this).attr('value'));
+  $('.menu-item').on('click', function() {
+    $(this).addClass('active');
+    $('.menu-item').not(this).removeClass('active');
+    $('#content').load('views/view.html #' + $(this).attr('value'));
   });
   
 
-  jQ('#weddingLink').on('click', function() {
+  $('#weddingLink').on('click', function() {
     console.log('clicked');
-    jQ('#content').load('views/view.html #ceremonies');
+    $('#content').load('views/view.html #ceremonies');
   });
 
   //Toggle Mobile Menu
-  jQ('.hamburger, .mobile-menu ul li').on('click', function () {
-    jQ('.mobile-menu').slideToggle();
+  $('.hamburger, .mobile-menu ul li').on('click', function () {
+    $('.mobile-menu').slideToggle();
   });
 })(jQuery) 
