8db14bb6c6de965de3e23b209eb9a4e781fe71b8
AaronAllan <aaron.a.pollock@gmail.com>
2015-09-05 12:55:33 -0400

    route 3.0

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -1,9 +1,11 @@
 console.log('available');
 
-page('/', function(){
+page('/#index', function(){
   console.log('index')
 });
 
 page('/#ceremonies', function () {
   console.log('ceremonies');
 });
+
+page.start();
