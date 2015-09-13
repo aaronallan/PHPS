1934a3bb1ad7b71edd87085d2f537d7241c3839b
AaronAllan <aaron.a.pollock@gmail.com>
2015-09-13 15:03:24 -0400

    simplified routing

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,14 +8,17 @@
   $('.menu-item').on('click', function() {
     $(this).addClass('active');
     $('.menu-item').not(this).removeClass('active');
-    $('#content').load('views/view.html #' + $(this).attr('value'));
+    // $('#content').load('views/view.html #' + $(this).attr('value'));
   });
   
-
-  $('#weddingLink').on('click', function() {
-    console.log('clicked');
-    $('#content').load('views/view.html #ceremonies');
+  $('a').on('click', function () {
+    $('#content').load('views/view.html #' + $(this).attr('value'));
   });
+
+  // $('#weddingLink').on('click', function() {
+  //   console.log('clicked');
+  //   $('#content').load('views/view.html #ceremonies');
+  // });
 
   //Toggle Mobile Menu
   $('.hamburger, .mobile-menu ul li').on('click', function () {
