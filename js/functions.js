93ae156e8f25b64e6b5bde029d1ff17f20fd5aa7
aaronallan <aaron.a.pollock@gmail.com>
2015-10-12 20:18:38 -0400

    added location modal

--- js/functions.js
+++ js/functions.js
@@ -3,5 +3,14 @@
     $('.hamburger, .mobile-menu ul li').on('click', function () {
       $('.mobile-menu').slideToggle();
     });
+
+    $('.icon').on('click', function () {
+      $('.modal').addClass('show');
+      $(this).hide();
+    });
+    $('.close').on('click', function () {
+      $('.modal').removeClass('show');
+      $('.icon').show();
+    });
   });
 })(jQuery);
